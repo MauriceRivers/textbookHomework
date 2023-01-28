18c8c3d0739b76343f06c88254e0896c9c823296
Maurice Rivers <mrivers5@ggc.edu>
2023-01-28 18:04:25 -0500

    added commented line.

---
+++ src/SpeedLight.java
@@ -0,0 +1,18 @@
+/**
+ * @Class Name: SpeedLight
+ * @Author: Maurice Rivers
+ * @Version: 1.0
+ * @Date: 26 January 2023
+ * Description: A program designed to calculate the speed of light
+ */
+
+
+
+
+
+public class SpeedLight {
+    public static void main(String[]args){
+        //complete here
+
+    }
+}
