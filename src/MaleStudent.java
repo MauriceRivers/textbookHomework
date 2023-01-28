91cc622b4604a11bdbc47edf3bcaf160c01bdbff
Maurice Rivers <mrivers5@ggc.edu>
2023-01-28 18:26:06 -0500

    add MaleStudent.java

---
+++ src/MaleStudent.java
@@ -0,0 +1,17 @@
+/**
+ * @Class: MaleStudent
+ * @Author: Maurice Rivers
+ * @Version: 1.0
+ * @Course: ITEC 2140 Section 05 Spring 2023
+ * @Written: 28 January 2023
+ *
+ * Description - Calculate the amount of male students
+ */
+
+
+
+
+
+
+public class MaleStudent {
+}
