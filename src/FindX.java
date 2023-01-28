a5d1133d8bfa58df195d8612c7307a0c4c54b65f
Maurice Rivers <mrivers5@ggc.edu>
2023-01-28 18:22:15 -0500

    add FindX.java

---
+++ src/FindX.java
@@ -0,0 +1,16 @@
+/**
+ * @Class: FindX
+ * @Author: Maurice Rivers
+ * @Version: 1.0
+ * @Course: ITEC 2140 Section 05 Spring 2023
+ * @Written: 28 January 2023
+ *
+ * Description - FindX
+ */
+
+
+
+
+
+public class FindX {
+}
