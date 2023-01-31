92d9db098cf19f88a15225f25a02ef55c10276a9
Maurice Rivers <mrivers5@ggc.edu>
2023-01-31 09:44:15 -0500

    Stamps.java Updated version number updated added variables updated description

--- src/Stamps.java
+++ src/Stamps.java
@@ -1,11 +1,11 @@
 /**
  * @Class: Stamps
  * @Author: Maurice Rivers
- * @Version: 1.0
+ * @Version: 1.1
  * @Course: ITEC 2140 Section 05 Spring 2023
  * @Written: 28 January 2023
  *
- * Description - Calculate the difference in stamps between Susan and Jean
+ * Description - this program Calculate the difference in stamps between Susan and Jean
  */
 
 
@@ -15,10 +15,12 @@
 
 public class Stamps {
     public static void main(String[]args){
-        System.out.println("Jean 40 racing car collection stamps - Susan 8 endangered animal collection stamps =");
-        System.out.print("Jean has ");
-        System.out.print(40 - 8);
-        System.out.println(" more stamps than Susan.");
+        int JeanStampCollection = 40;
+        int SusanStampCollection = 8;
+        int x = JeanStampCollection - SusanStampCollection;
+        System.out.println("Jean has " + x +
+                " more stamps than Susan");
+
     }
 
 
