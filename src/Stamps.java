bccda09aec6e8cc0de5e4c5d1e0d1343a3afffe4
Maurice Rivers <mrivers5@ggc.edu>
2023-01-31 01:46:15 -0500

    Stamps.java description updated

--- src/Stamps.java
+++ src/Stamps.java
@@ -5,7 +5,7 @@
  * @Course: ITEC 2140 Section 05 Spring 2023
  * @Written: 28 January 2023
  *
- * Description - Stamps
+ * Description - Calculate the difference in stamps between Susan and Jean
  */
 
 
