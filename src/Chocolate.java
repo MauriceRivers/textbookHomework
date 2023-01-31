a3fefdcf2670ed2093920195b5e3673b5cedb05c
Maurice Rivers <mrivers5@ggc.edu>
2023-01-31 01:42:14 -0500

    Chocolate.java calculations and outputs added

--- src/Chocolate.java
+++ src/Chocolate.java
@@ -12,4 +12,12 @@
 
 
 public class Chocolate {
+    public static void main(String[]args){
+        System.out.println("9 bags * 2 chocolate bars per bag =");
+        System.out.println(9 * 2 + " chocolate bars");
+        System.out.println("18 chocolate bars / 3 chocolate bars per bag = ");
+        System.out.println(18 / 3 + " bags of chocolate bars");
+    }
+
+
 }
