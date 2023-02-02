a0034f2908c535eff50f9db3297d9b3f8a25ab5c
Maurice Rivers <mrivers5@ggc.edu>
2023-02-01 20:24:30 -0500

    Circle.java Version updated Scanner added to program

--- src/Circle.java
+++ src/Circle.java
@@ -1,24 +1,28 @@
 /**
  * @Class: Circle
  * @Author: Maurice Rivers
- * @Version: 1.0
+ * @Version: 1.1
  * @Course: ITEC 2140 Section 05 Spring 2023
  * @Written: 28 January 2023
  *
- * Description - Calculate the area and perimeter of a circle with a radius of 9.5
+ * Description - Program to Calculate the area and perimeter of a circle
  */
 
 
 
 
 
-
+import java.util.Scanner;
 public class Circle {
     public static void main(String[]args){
-        double x = 9.5;
-        double area = x * x * Math.PI;
-        double perimeter = 2 * x * Math.PI;
-        System.out.println("Area of a circle with the radius of 9.5 = " + area);
-        System.out.println("Perimeter of a circle with the radius of 9.5 = " + perimeter);
+
+        java.util.Scanner Input = new java.util.Scanner(System.in);
+        System.out.print("Enter the radius of your circle: ");
+
+        double radius = Input.nextDouble();
+        double area = radius * radius * Math.PI;
+        double perimeter = 2 * radius * Math.PI;
+        System.out.println("Area of a circle with the radius of " + radius + " = " + area);
+        System.out.println("Perimeter of a circle with the radius of " + radius + " = " + perimeter);
     }
 }
