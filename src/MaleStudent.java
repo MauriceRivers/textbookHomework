d1ca33fbb4272e5fd0d1d82c552da7e052c70275
Maurice Rivers <mrivers5@ggc.edu>
2023-02-02 22:02:19 -0500

    MaleStudent.java Scanner added Version updated

--- src/MaleStudent.java
+++ src/MaleStudent.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
+
 /**
  * @Class: MaleStudent
  * @Author: Maurice Rivers
- * @Version: 1.0
+ * @Version: 1.1
  * @Course: ITEC 2140 Section 05 Spring 2023
  * @Written: 28 January 2023
  *
- * Description - Calculate the amount of male students
+ * Description - Calculate the amount of male students in the middle school
  */
 
 
@@ -15,9 +17,16 @@
 
 public class MaleStudent {
     public static void main(String[]args){
-        System.out.print("389 Total Students - 175 Female Students = ");
-        System.out.println(389 - 175);
-        System.out.println("There are 214 Male Students");
+        java.util.Scanner TotalStudent = new java.util.Scanner(System.in);
+        System.out.print("How many total students are there: ");
+        int Students = TotalStudent.nextInt();
+
+        System.out.print(" How many Female Students are there: ");
+        int FemaleStudents = TotalStudent.nextInt();
+
+        int MaleStudent = Students - FemaleStudents;
+        System.out.println("There are " + MaleStudent + " Male students in the Middle School");
+
 
     }
 }
